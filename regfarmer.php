f371992cc34d79075a7c3b3f4b6399d861e90aa0
vedavallika <vedavallika@gmail.com>
2023-11-25 18:18:53 +0530

    edited

--- regfarmer.php
+++ regfarmer.php
@@ -15,6 +15,10 @@
 		</div>
 		<div class="content">
 			<?php
+			ini_set('display_errors', 1);
+			ini_set('display_startup_errors', 1);
+			error_reporting(E_ALL);
+			
 				$host = "database-1.coannvnsp2rk.ap-south-1.rds.amazonaws.com";
 				$user = "root";
 				$pass = "";
