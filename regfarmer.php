457390d82de3c5ed56946de0d4398aa2083b32cf
vedavallika <vedavallika@gmail.com>
2023-11-25 19:13:06 +0530

    edited

--- regfarmer.php
+++ regfarmer.php
@@ -19,7 +19,7 @@
 			ini_set('display_startup_errors', 1);
 			error_reporting(E_ALL);
 
-				$host = "veda.coannvnsp2rk.ap-south-1.rds.amazonaws.com";
+				$host = "localhost";
 				$user = "root";
 				$pass = "";
 				$db = "veda";
