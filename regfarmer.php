08ed6d9f114b7656c3391a5ab4a56172aa3665fe
vedavallika <vedavallika@gmail.com>
2023-11-25 19:11:02 +0530

    edited

--- regfarmer.php
+++ regfarmer.php
@@ -23,8 +23,9 @@
 				$user = "root";
 				$pass = "";
 				$db = "veda";
+				$port = 3306;
 				
-				$con = mysqli_connect($host, $user, $pass, $db,'3306');
+				$con = mysqli_connect($host, $user, $pass, $db,$port);
 				
 				if (!$con) {
 					die("Connection failed: " . mysqli_connect_error());
